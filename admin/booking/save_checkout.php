ad2718635a70c37625a7b76212d08ef9ad5a682c
Nantawat Siriwan <tongfs33@gmail.com>
2018-10-05 01:27:55 +0700

    now can stored order to database

---
+++ admin/booking/save_checkout.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+$serverName = "localhost";
+$userName = "root";
+$userPassword = "";
+$dbName = "my_project";
+
+$objCon = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+if (!$objCon) {
+	echo $objCon->connect_error;
+	exit();
+}
+
+  $Total = 0;
+  $SumTotal = 0;
+
+  $strSQL = "
+	INSERT INTO orders (OrderDate,customer_id)
+	VALUES
+	('".date("Y-m-d H:i:s")."','".$_POST["p_id"]."') 
+  ";
+  $objQuery = mysqli_query($objCon,$strSQL);
+  if(!$objQuery)
+  {
+	echo $objCon->error;
+	exit();
+  }
+
+  $strOrderID = mysqli_insert_id($objCon);
+
+  for($i=0;$i<=(int)$_SESSION["intLine"];$i++)
+  {
+	  if($_SESSION["strProductID"][$i] != "")
+	  {
+			  $strSQL = "
+				INSERT INTO orders_detail (OrderID,ProductID,Qty)
+				VALUES
+				('".$strOrderID."','".$_SESSION["strProductID"][$i]."','".$_SESSION["strQty"][$i]."') 
+			  ";
+			  mysqli_query($objCon,$strSQL);
+	  }
+  }
+
+mysqli_close($objCon);
+
+session_destroy();
+
+header("location:finish_order.php?OrderID=".$strOrderID);
+?>
+
+<?php /* This code download from www.ThaiCreate.Com */ ?>
